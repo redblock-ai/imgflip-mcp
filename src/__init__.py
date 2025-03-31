ca54fc4c098bfb8668bba7eaf7c38eaf56b2f843
basrizk <basem.rizk@outlook.com>
2025-03-31 12:55:01 -0700

    import env variables from dotenv file if present

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,11 @@ A Model Control Protocol server for creating memes with the Imgflip API.
 """
 
 __version__ = "1.0.0"
+
+from dotenv import load_dotenv
+found_env_file = load_dotenv(override=True)  # take environment variables
+
+if found_env_file:
+    print("Environment variables loaded from .env file.")
+else:
+    print("No .env file found. Using system environment variables.")
